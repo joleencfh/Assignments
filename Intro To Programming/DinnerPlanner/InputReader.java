f99ca88e300aa5cbb71684e3e99d24f290463fb8
Giulia Consonni <kawngc@gmail.com>
2018-06-06 17:05:59 +0200

    Update InputReader.java

--- Intro To Programming/DinnerPlanner/InputReader.java
+++ Intro To Programming/DinnerPlanner/InputReader.java
@@ -1,14 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- * InputReader reads typed text input from the standard text terminal. 
- * The text typed by a user is then chopped into words, and a set of words 
- * is provided.
- * 
- * @author     Michael Kölling and David J. Barnes
- * @version    1.0 (2016.02.29)
- */
+
 public class InputReader
 {
     private Scanner reader;
@@ -21,13 +14,7 @@ public class InputReader
         reader = new Scanner(System.in);
     }
 
-    /**
-     * Read a line of text from standard input (the text terminal),
-     * and return it as a set of words.
-     *
-     * @return  A set of Strings, where each String is one of the 
-     *          words typed by the user
-     */
+   
     public ArrayList<String> getInput() 
     {
         System.out.print("> ");                
